d81e54640925f113f62e069c971014c5f04dbe5d
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2023-02-25 01:26:15 +0100

    init: Add folder structure

---
+++ cs_frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+/***
+ * This file is used to export all the pages in the application.
+ * This is done to make it easier to import pages in other files.
+ */
+
+import Home from "./home";
+import Navigation from "./navigation";
+import HandlingDevice from "./handlingDevice";
+import ManualTask from "./manualNavigation";
+import Science from "./science";
+import Logs from "./logs";
+
+export { Home, Navigation, HandlingDevice, ManualTask, Science, Logs };
