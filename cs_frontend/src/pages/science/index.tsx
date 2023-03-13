1b9f4e8931750d115dd27738c4e092e9e7c546b8
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2023-03-13 19:50:17 +0100

    feat: add WorkInProgress icon

--- cs_frontend/src/pages/science/index.tsx
+++ cs_frontend/src/pages/science/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import BackButton from "../../components/BackButton";
 import Background from "../../components/Background";
+import WorkInProgress from "../../components/WorkInProgress";
 
 export default () => {
 	return (
-		<div>
+		<div className="page">
 			<Background />
+			<BackButton />
+			<WorkInProgress />
 		</div>
 	);
 };
