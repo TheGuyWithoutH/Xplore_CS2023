dadcf63917fb8846642355d35e1116d6e6bb449f
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2023-03-13 19:51:17 +0100

    feat: finish timer

--- cs_frontend/src/components/Timer/index.tsx
+++ cs_frontend/src/components/Timer/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import styles from "./style.module.sass";
 import { Status } from "../../utils/status.type";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import PauseIcon from "@mui/icons-material/Pause";
-import Replay10Icon from "@mui/icons-material/Replay10";
-import PersonIcon from "@mui/icons-material/Person";
+import PlayArrowIcon from "@mui/icons-material/PlayArrowRounded";
+import PauseIcon from "@mui/icons-material/PauseRounded";
+import Replay10Icon from "@mui/icons-material/Replay10Rounded";
+import PersonIcon from "@mui/icons-material/PersonRounded";
+import PeopleAltRoundedIcon from "@mui/icons-material/PeopleAltRounded";
 import {
 	Battery2Bar,
 	Battery0Bar,
@@ -13,7 +14,7 @@ import {
 	Battery4Bar,
 	Battery5Bar,
 	Battery6Bar,
-	BatteryFull,
+	BatteryFullRounded,
 } from "@mui/icons-material";
 
 const Timer = ({
@@ -29,14 +30,16 @@ const Timer = ({
 	connection?: number;
 	battery?: number;
 }) => {
-	const [endTime, setEndTime] = useState(0); // end time in milliseconds
 	const [minutes, setMinutes] = useState(0); // minutes left
 	const [seconds, setSeconds] = useState(0); // seconds left
 	const [finished, setFinished] = useState(false);
 	const [active, setActive] = useState(true);
+	let interval: NodeJS.Timer;
 
-	const getTime = () => {
-		const time = endTime - Date.now();
+	const getTime = (changeMinutes?: number, changeSeconds?: number) => {
+		let newMinutes = changeMinutes || minutes;
+		let newSeconds = changeSeconds || seconds;
+		let time = newMinutes * 60000 + newSeconds * 1000;
 
 		if (time <= 0) {
 			setFinished(true);
@@ -45,30 +48,37 @@ const Timer = ({
 			return;
 		}
 
+		if (active) time -= 1000;
+
 		setMinutes(Math.floor((time / 1000 / 60) % 60));
 		setSeconds(Math.floor((time / 1000) % 60));
 		setFinished(false);
 	};
 
 	useEffect(() => {
-		let interval: NodeJS.Timer;
 		if (!finished && active) {
-			interval = setInterval(() => getTime(), 1000);
+			interval = setTimeout(() => getTime(), 1000);
 		}
 
-		return () => clearInterval(interval);
-	}, [endTime, finished, active]);
+		return () => clearTimeout(interval);
+	}, [finished, active, minutes, seconds]);
 
 	const changeTime = (minutes: number, seconds: number) => {
-		if (minutes >= 0 && seconds >= 0 && seconds < 60)
-			setEndTime(Date.now() + minutes * 60000 + seconds * 1000);
-		getTime();
+		if (minutes >= 0 && seconds >= 0 && seconds < 60) {
+			clearTimeout(interval);
+			getTime(minutes, seconds);
+		} else if (minutes >= 0 && seconds >= 60) {
+			clearTimeout(interval);
+			getTime(
+				minutes + Math.floor(seconds / 60),
+				(seconds - Math.floor(seconds / 60) * 60) % 60
+			);
+		}
 	};
 
 	React.useEffect(() => {
 		if (finished && onFinished) {
 			onFinished();
-			console.log("finished");
 		}
 	}, [finished, onFinished]);
 
@@ -76,22 +86,42 @@ const Timer = ({
 		<div className={styles.timer}>
 			<div className={styles.batteryInfos}>
 				<p className={styles.battery}>{battery}%</p>
-				<div className={styles.batteryIcon}>
-					<BatteryFull className={styles.icon} />
-				</div>
+				<div className={styles.batteryIcon}>{getBatteryState(battery)}</div>
 			</div>
 			<div className={styles.timeInfos}>
 				<div className={styles.time}>
 					<input
 						type="text"
-						value={timeRepresentation(minutes)}
+						maxLength={2}
+						value={timeRepresentation(minutes, active)}
+						onFocus={(e) => {
+							e.target.value = "";
+							setActive(false);
+						}}
+						onBlur={(e) => {
+							if (e.target.value === "") {
+								changeTime(0, seconds);
+							}
+							setActive(true);
+						}}
 						onChange={(e) => changeTime(parseInt(e.target.value), seconds)}
 						className={styles.input}
 					/>
 					<p className={styles.comma}>:</p>
 					<input
 						type="text"
-						value={timeRepresentation(seconds)}
+						maxLength={2}
+						value={timeRepresentation(seconds, active)}
+						onFocus={(e) => {
+							e.target.value = "";
+							setActive(false);
+						}}
+						onBlur={(e) => {
+							if (e.target.value === "") {
+								changeTime(minutes, 0);
+							}
+							setActive(true);
+						}}
 						onChange={(e) => changeTime(minutes, parseInt(e.target.value))}
 						className={styles.input}
 					/>
@@ -116,7 +146,11 @@ const Timer = ({
 			<div className={styles.connections}>
 				<p className={styles.connectionsText}>{connection}</p>
 				<div className={styles.connectionsIcon}>
-					<PersonIcon className={styles.icon} />
+					{connection > 1 ? (
+						<PeopleAltRoundedIcon className={styles.icon} />
+					) : (
+						<PersonIcon className={styles.icon} />
+					)}
 				</div>
 			</div>
 		</div>
@@ -127,10 +161,30 @@ export default Timer;
 
 ////////////////////////////METHODS///////////////////////////
 
-const timeRepresentation = (time: number) => {
-	if (time < 10) {
+const timeRepresentation = (time: number, active = true) => {
+	if (time < 10 && active) {
 		return `0${time}`;
 	} else {
 		return time.toString();
+	}
+};
+
+const getBatteryState = (battery: number) => {
+	if (battery < 12.5) {
+		return <Battery0Bar className={styles.icon} />;
+	} else if (battery < 25) {
+		return <Battery1Bar className={styles.icon} />;
+	} else if (battery < 37.5) {
+		return <Battery2Bar className={styles.icon} />;
+	} else if (battery < 50) {
+		return <Battery3Bar className={styles.icon} />;
+	} else if (battery < 62.5) {
+		return <Battery4Bar className={styles.icon} />;
+	} else if (battery < 75) {
+		return <Battery5Bar className={styles.icon} />;
+	} else if (battery < 87.5) {
+		return <Battery6Bar className={styles.icon} />;
+	} else {
+		return <BatteryFullRounded className={styles.icon} />;
 	}
 };
