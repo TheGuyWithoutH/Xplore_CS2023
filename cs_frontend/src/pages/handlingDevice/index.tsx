a36890bf73bd1f517409863acaee8e98c7406738
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2023-03-18 01:35:54 +0100

    feat: add a not available component

--- cs_frontend/src/pages/handlingDevice/index.tsx
+++ cs_frontend/src/pages/handlingDevice/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import BackButton from "../../components/BackButton";
 import Background from "../../components/Background";
+import NotAvailable from "../../components/NotAvailable";
 
 export default () => {
 	return (
-		<div>
+		<div className="page">
 			<Background />
+			<BackButton />
+			<NotAvailable />
 		</div>
 	);
 };
