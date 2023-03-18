2e08efcf9c705baaf32e7b0319dfe42f10116d87
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2023-03-18 01:36:19 +0100

    feat: refactoring some pages

--- cs_frontend/src/pages/camera/index.tsx
+++ cs_frontend/src/pages/camera/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import BackButton from "../../components/BackButton";
 import Background from "../../components/Background";
 
 export default () => {
 	return (
-		<div>
+		<div className="page center">
 			<Background />
+			<BackButton />
 		</div>
 	);
 };
